ee8166fa11945659a3ae3573492c3a49f574d6c0
Christian Montero <cmontero@bu.edu>
2019-09-29 13:00:03 -0400

    Update unitTests.py

--- unitTests.py
+++ unitTests.py
@@ -15,19 +15,6 @@ badInd, badFam = parseFile(dataBad)
 ## Tests all Age Related User Stories
 class TestAge(unittest.TestCase):
 
-    ## US01 Test: Dates before Current Date
-    def testCurrDate_Pass(self):
-        for fam1 in goodFam:
-            x = checkErr.checkCurrDate(fam1, 0, [], [])
-            self.assertFalse(x)
-
-    def testCurrDate_Fail(self):
-        res = []
-        for fam2 in badFam:
-            x = checkErr.checkCurrDate(fam2, 0, [], [])
-            res.append(x)
-        self.assertIn(True, res)
-
     ## US02 Test: Ensure Birth Date is before Marriage Date
     def testBirth_marriage_Pass(self):
         for fam1 in goodFam:
@@ -46,36 +33,25 @@ class TestAge(unittest.TestCase):
             x = checkErr.checkBirth_death(ind1, 0, [])
             if x != None:
                 self.assertFalse(x)
-        
+ 
     def testCheckBirth_death_Fail(self):
         for ind1 in badInd:
             x = checkErr.checkBirth_death(ind1, 0, [])
             if x != None:
                 self.assertTrue(x)
 
-    ## US04 Test: Marriage before Divorce
-    def testMarriage_Divorce_Pass(self):
-        for fam1 in goodFam:
-            x = checkErr.checkMarrBeforeDiv(fam1, 0, [])
-            self.assertFalse(x)
-
-    def testMarriage_Divorce_Fail(self):
-        res = []
-        for fam2 in badFam:
-            x = checkErr.checkMarrBeforeDiv(fam2, 0, [])
-            res.append(x)
-        self.assertIn(True, res)
-
     ## US05 Test: Marriage before death
     def testMarriage_death_Pass(self):
+        res = []
         for fam2 in goodFam:
             x = checkErr.checkMarriage(fam2, 0, [], goodInd)
             self.assertFalse(x)
+           
 
     def testMarriage_death_Fail(self):
         res = []
         for fam2 in badFam:
-            x = checkErr.checkMarriage(fam2, 0, [], goodInd)
+            x = checkErr.checkMarriage(fam2, 0, [], badInd)
             res.append(x)
         self.assertIn(True, res)
 
@@ -104,7 +80,6 @@ class TestAge(unittest.TestCase):
             res.append(x)
         self.assertIn(True, res)
     
-    
     ## US08 Test: Check Birth is after Parent Marriage
     def testBirth_parentMarriage_Pass(self):
         for fam1 in goodFam:
@@ -125,3 +100,4 @@ if __name__ == '__main__':
 
     suite = unittest.TestLoader().loadTestsFromTestCase(TestAge)
     unittest.TextTestRunner(verbosity = 2).run(suite)
+    
